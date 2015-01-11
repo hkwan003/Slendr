ef51d2361866f61a492a74073d4a29fa6add852e
Tricia Hinkson <tryctor@gmail.com>
2015-01-10 17:37:52 -0800

    comments

--- app/src/main/java/com/example/calvin/slendr/SkillsPage.java
+++ app/src/main/java/com/example/calvin/slendr/SkillsPage.java
@@ -3,22 +3,21 @@ package com.example.calvin.slendr;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-//import android.support.v7.widget.;
+import android.support.v7.widget.RecyclerView;
+
 
 /**
  * Created by Tricia on 1/10/2015.
  */
 public class SkillsPage extends Activity {
-/*
     private RecyclerView recView;
     private RecyclerView.Adapter recAdapter;
     private RecyclerView.LayoutManager recyclerManager;
-*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_skills);
+        setContentView(R.layout.);
 
         Intent intent = getIntent();
     }
