a85aecf3656f3ecb719768388c2579fac068816c
Hech <tryctor@gmail.com>
2015-01-18 11:55:17 -0800

    toolbar welcome

--- app/src/main/java/com/example/calvin/slendr/SkillsActivity.java
+++ app/src/main/java/com/example/calvin/slendr/SkillsActivity.java
@@ -1,13 +1,15 @@
 package com.example.calvin.slendr;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.support.annotation.ColorRes;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-
-import java.util.ArrayList;
-
 
 /**
  * Created by Tricia on 1/10/2015.
@@ -17,11 +19,14 @@ public class SkillsActivity extends Activity {
     private RecyclerView.Adapter recAdapter;
     private RecyclerView.LayoutManager recManager;
     String [] defaultset = {"Example Language", "Example Design"};
-    ArrayList<String> customset;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skills);
+        //UI Changes
+
+        //bar.setBackgroundDrawable(new ColorDrawable(ColorDrawable));
 
         Intent intent = getIntent();
         recView = (RecyclerView) findViewById(R.id.skill_cycle);
